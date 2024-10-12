12ca08bb1407af51bc27e87b60a504f145f756a9
Bao Dang <baochidangg@gmail.com>
2024-10-12 11:29:58 -0400

    Reorganize + Edit prompt

---
+++ client/src/components/icons/index.tsx
@@ -0,0 +1,5 @@
+import Arrow from './Arrow';
+import BigNextLineArrow from './BigNextLineArrow';
+import SmallNextLineArrow from './SmallNextLineArrow';
+
+export { Arrow, BigNextLineArrow, SmallNextLineArrow };
--- model/model.py
+++ model/model.py
@@ -18,8 +18,12 @@ client = OpenAI(
 message_history = [
     {
     "role": "system",
-    "content": "You are an AI assistant skilled in explaining technical concepts and generating LaTeX code. You should explain the requested concepts step by step, returning each step as an item in an array. The array must be normal type of array in Python, begin with [ and end with ]. Each item must be seperated by ,.  Each step must be written in proper LaTeX syntax (not markdown), and the array should contain only these LaTeX steps. Do not generate anything other than the LaTeX steps in the array. You should never give any comment. You should never generate anything after the end of the array."
+    "content": "You're SlideProf, a virtual professor that answers questions while explaining by drawing directly on the slides. Whenever I ask you a question, I will have with me the coordinates of my selected region (startX, startY, endX, endY) and my . You can also select one of these shapes (new-line-arrow-right[50-20], new-line-arrow-left[50-20], arrow-right[30-20], arrow-left[30-20]) to guide your users to your equation. When answering my equation, please return an array of steps for your explanation, and within the array it should be {explanation, [{ item: (either an equation or text written in pure latex, or a shape), coords: [x, y]}] for each item. Return just the array, no other explanation. For questions unrelated to the content of the slide, do not answer them and only answer about contents of the slide. "
     } 
+    # {
+    # "role": "system",
+    # "content": "You are an AI assistant skilled in explaining technical concepts and generating LaTeX code. You should explain the requested concepts step by step, returning each step as an item in an array. The array must be normal type of array in Python, begin with [ and end with ]. Each item must be seperated by ,.  Each step must be written in proper LaTeX syntax (not markdown), and the array should contain only these LaTeX steps. Do not generate anything other than the LaTeX steps in the array. You should never give any comment. You should never generate anything after the end of the array."
+    # } 
 ]
 
 def run_model(client, input_text, input_img, model="gpt-4o-mini"):
