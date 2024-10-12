4c04e9a411d26eeb0c67c0dbbb35da3a5f9c37bf
Hung Nguyen Pro <htn@Hungs-MacBook-Pro.local>
2024-10-12 15:15:54 -0400

    Change extract img input args

--- model/utils.py
+++ model/utils.py
@@ -1,15 +1,45 @@
+# import fitz
+# from PIL import Image
+# import io
+
+# def extract_image_from_pdf(pdf_path, page_number, rect):
+#     """
+#     Extract an image from a specific coordinate on a given page in a PDF file.
+
+#     Args:
+#         pdf_path (str): Path to the PDF file.
+#         page_number (int): Page number to extract image from (0-indexed).
+#         rect (tuple): A tuple with the coordinates of the rectangle (x0, y0, x1, y1).
+
+#     Returns:
+#         PIL.Image.Image: The extracted image from the specified rectangle.
+#     """
+#     document = fitz.open(pdf_path)
+    
+#     page = document.load_page(page_number)
+#     rectangle = fitz.Rect(rect)
+#     pix = page.get_pixmap(clip=rectangle)
+    
+#     document.close()
+#     image_data = pix.tobytes("png")
+#     image = Image.open(io.BytesIO(image_data))
+    
+#     return image
+
+
+
 import fitz
 from PIL import Image
 import io
 
-def extract_image_from_pdf(pdf_path, page_number, rect):
+def extract_image_from_pdf(pdf_path, page_number, pixel):
     """
-    Extract an image from a specific coordinate on a given page in a PDF file.
+    Extract an image from a specific pixel coordinate on a given page in a PDF file.
 
     Args:
         pdf_path (str): Path to the PDF file.
         page_number (int): Page number to extract image from (0-indexed).
-        rect (tuple): A tuple with the coordinates of the rectangle (x0, y0, x1, y1).
+        pixel (tuple): A tuple with the coordinates of the rectangle in pixels (x0, y0, x1, y1).
 
     Returns:
         PIL.Image.Image: The extracted image from the specified rectangle.
@@ -17,8 +47,8 @@ def extract_image_from_pdf(pdf_path, page_number, rect):
     document = fitz.open(pdf_path)
     
     page = document.load_page(page_number)
-    rectangle = fitz.Rect(rect)
-    pix = page.get_pixmap(clip=rectangle)
+    rect = fitz.Rect(pixel)
+    pix = page.get_pixmap(clip=rect)
     
     document.close()
     image_data = pix.tobytes("png")
